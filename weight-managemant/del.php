3c1e215ef7046772484ae2bacd47caade93ed90b
shun67 <52106552+shun67@users.noreply.github.com>
2019-12-28 01:06:16 +0900

    Update del.php

--- weight-managemant/del.php
+++ weight-managemant/del.php
@@ -1,7 +1,7 @@
 <?php
-$dsn = 'mysql:dbname=tb210005db;host=localhost';
-$user = 'tb-210005';
-$password = 'uKdsWMXhMN';
+$dsn = 'mysql:dbname=*********;host=localhost';
+$user = '*********';
+$password = '*********';
 //データベースの作成（IF NOT EXISTS）
 $sql = 'DROP DATABASE member';
   //name comment date pass
